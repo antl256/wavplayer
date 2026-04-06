0fb4f916c26e092a25eb8992d13643f208e66ca8
anton <antonbranstedt@telia.com>
2026-04-06 22:53:43 +0200

    add missing header in logging file

--- src/wp_logging.h
+++ src/wp_logging.h
@@ -3,6 +3,8 @@
 
 #include "wp_constants.h"
 
+#include <stdio.h>
+
 #define wp_log_info(format, ...)           fprintf(stdout, WP_NAME ": [info] " format "\n", ##__VA_ARGS__)
 #define wp_log_warning(format, ...)        fprintf(stdout, WP_NAME ": \033[33m[warning]\033[0m " format "\n", ##__VA_ARGS__)
 #define wp_log_error(format, ...)          fprintf(stderr, WP_NAME ": \033[31m[error]\033[0m " format "\n", ##__VA_ARGS__)
